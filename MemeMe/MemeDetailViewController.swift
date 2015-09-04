fc71b823e5aba45ed3e46f5ae3dc2cbe3df2ef18
ocedeno <octavio.cedeno@me.com>
2015-09-03 20:19:32 -0400

    Added detailViewController view and swift file

---
+++ MemeMe/MemeDetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MemeDetailViewController.swift
+//  MemeMe
+//
+//  Created by Programming on 9/3/15.
+//  Copyright (c) 2015 Cedeno Enterprise, Inc. All rights reserved.
+//
+
+import UIKit
+
+class MemeDetailViewController: UIViewController {
+
+    @IBOutlet weak var memeViewImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
+
